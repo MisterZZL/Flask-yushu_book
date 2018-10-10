654d14699b4d1b8a35a7b51276b57ce05a51a3f6
Json <619246759@qq.com>
2018-10-11 00:20:12 +0800

    Signed-off-by: Json <619246759@qq.com> 1、实现首页展示效果（最近上传菜单）

--- app/models/gitf.py
+++ app/models/gitf.py
@@ -1,6 +1,9 @@
-from sqlalchemy import Column, Integer, Boolean, ForeignKey, String, SmallInteger
+from flask import current_app
+from sqlalchemy import Column, Integer, Boolean, ForeignKey, String, SmallInteger, desc
 from sqlalchemy.orm import relationship
 from app.models.base import base
+from app.spider.yushu_book import YuShu_Book
+from app.view_models.book import BookViewModel
 
 
 class Gift(base):
@@ -11,5 +14,25 @@ class Gift(base):
     # bid = Column(Integer,ForeignKey('book.id'))
     isbn = Column(String(15), nullable=False)
     launched = Column(Boolean, default=False)
+
+    @staticmethod
+    def recent():
+        #差赠送清单列表
+        #launched = False，False表示没有送出的书
+        #group_by(Gift.isbn),按照isbn号，把不同的人送的同一本书分到一个组，目的是去重
+        #order_by(desc(Gift.create_time))，order_by排序，desc()是降序排列，Gift.create_time赠送的时间
+        #按照赠送的时间降序排列
+        #limit(current_app.config['RECENT_BOOK_COUNTS'])，限制页面最多展示的数量
+        #distinct()，去重
+        recent_gifts = Gift.query.filter_by(launched = False).group_by(
+            Gift.isbn).order_by(desc(Gift.create_time)).limit(
+            current_app.config['RECENT_BOOK_COUNTS']).distinct().all()
+        return recent_gifts
+
+    @property
+    def books(self):
+        date = YuShu_Book.search_by_isbn(self.isbn)     #isbn号找书，得到书详情数据date
+        book = BookViewModel.handle_book_date(date)     #通过单本书的方法处理date，提取需要的信息
+        return book
 
 
--- app/web/main.py
+++ app/web/main.py
@@ -1,9 +1,19 @@
+from flask import render_template
+
 from . import web
 
 
+#首页展示  最近上传
 @web.route('/')
 def index():
-    return 'index'
+    from app.models.gitf import Gift
+    recent_gifts = Gift.recent()                    #调用Gift.recent()方法获得最近上传的书
+    books = []
+    # for gift in recent_gifts:
+    #     books.append(Gift.books(gift))
+    # books = [gift.books() for gift in recent_gifts]
+    books = [gift.books for gift in recent_gifts]   #调用Gift.books方法，获取最近上传的书的详情
+    return render_template('index.html',recent = books)
 
 
 @web.route('/personal')

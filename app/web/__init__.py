f11054c413489456701da2949b478768434c513f
MisterZZL <zhangzhenlin322@163.com>
2018-10-13 17:04:48 +0800

    Signed-off-by: MisterZZL <zhangzhenlin322@163.com> 1、完成进入忘记密码页面，输入用户自己的邮箱，先对邮箱进行数据校验， 看是否是正确的邮箱格式； 2、数据库查找对应的邮箱信息，匹配到，说明用户存在，否则，该邮箱没有注册； 3、对于查到未注册邮箱，暂时用404页面处理； 4、查找邮箱时用到first_or_404方法，以及面向切面的思想，具体可见查看笔记

--- app/web/__init__.py
+++ app/web/__init__.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python 
 # -*- coding:utf-8 -*-
-from flask import blueprints
+from flask import blueprints, render_template
 
 web = blueprints.Blueprint('web',__name__)
 
+@web.app_errorhandler(404)
+def not_found(e):
+    return render_template('404.html')
+
 from app.web import book,auth,drift,gift,main,test,wish
+
